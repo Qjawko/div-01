c2fffeb78bbd43a5066390d297ed27af2a242734
Qjawko <Qjawko@gmail.com>
2020-01-09 15:50:38 +0600

    added commments

--- ListRemoveIf.go
+++ ListRemoveIf.go
@@ -11,20 +11,21 @@ type List struct {
 }
 
 func ListPushBack(l *List, data interface{}) {
-	node := &NodeL{data, nil}
+	node := &NodeL{data, nil} //создаем элемента листа который мы хотим вставить в конец
 
-	if l.Head == nil {
-		l.Head = node
-		return
+	if l.Head == nil { //если начало листа nil
+		l.Head = node //делаем наш node как начало
+		return //конец работы функции
 	}
 
-	l.Tail = node
-	iter := l.Head
-	for iter.Next != nil {
-		iter = iter.Next
+	l.Tail = node //?_?
+	iter := l.Head //создаем итератор с помощью которого мы будем ходить по листу
+	for iter.Next != nil { //пока следующий элемент после iter не будет равен nil
+		iter = iter.Next //идем дальше по листу
 	}
-	iter.Next = node
-}
+	iter.Next = node //после того как закончился цикл, следующий эелемент после iter равен nil 
+			 //соответсвенно это значит что это последний элемент в list, вставляем наш node следующим после последнего элемента
+} //конец функции
 
 func ListRemoveIf(l *List, data_ref interface{}) {
 	newList := &List{}
